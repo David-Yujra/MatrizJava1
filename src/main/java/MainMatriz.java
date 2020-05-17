74075e602ee41f909b638dfd60a2309ce5c3d911
David Yujra <att.mili_darco@hotmail.es>
2020-05-17 15:45:30 -0400

    Cambio funcion crear para sumar matrices

--- src/main/java/MainMatriz.java
+++ src/main/java/MainMatriz.java
@@ -1,7 +1,9 @@
 public class MainMatriz {
     public static void main(String[] args){
         Matriz ma=new Matriz();
-        int [][]m=ma.crear();
-        ma.mostrar(m);
+
+        int [][]m1=ma.crear(2,2);
+        int [][]m2=ma.crear(2,2);
+        ma.mostrar(ma.sumar(m1,m2));
     }
 }
--- src/main/java/Matriz.java
+++ src/main/java/Matriz.java
@@ -1,21 +1,45 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Matriz {
-    public int[][] crear() {
-        int m1[][] = new int[2][2];
-        int n=1;
+    public int[][] crear(int fil, int col) {
+        int m1[][] = new int[fil][col];
+        Scanner leer = new Scanner(System.in);
         for (int i = 0; i < m1.length; i++) {
             for (int j = 0; j < m1[0].length; j++) {
-                m1[i][j] = n++;
+                System.out.println("Ingrese la fila " + (i + 1) + " columna " + (j + 1));
+                m1[i][j] = leer.nextInt();
             }
         }
 
         return m1;
     }
-    public void mostrar(int[][] m){
+
+    public int[][] sumar(int[][] m1, int[][] m2) {
+        if (m1.length == m2.length && m1[0].length == m2[0].length) {
+            int[][] mr = new int[m1.length][m1[0].length];
+            for (int i = 0; i < mr.length; i++) {
+                for (int j = 0; j < mr[0].length; j++) {
+                    mr[i][j] = m1[i][j] + m2[i][j];
+                }
+            }
+            return mr;
+        }
+        else{
+            System.out.println("Las matrices deben ser de dimensiones iguales");
+            return null;
+        }
+
+    }
+
+
+    public void mostrar(int[][] m) {
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[0].length; j++) {
-                System.out.print(m[i][j]+"\t");
+                System.out.print(m[i][j] + "\t\t\t");
             }
-            System.out.println( );
+            System.out.println();
         }
     }
 }
